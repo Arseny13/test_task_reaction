63d271792ed2d4c55e7a2faae894e5111b627804
Arseny <arsenyxiii@gmail.com>
2023-11-08 15:03:45 +0300

    add model Blog

--- app/blog/models.py
+++ app/blog/models.py
@@ -1,5 +1,30 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
 
 
 class Blog(models.Model):
-    pass
+    """Класс блога"""
+    user = models.ForeignKey(
+        User,
+        verbose_name='Пользователь',
+        on_delete=models.CASCADE,
+        related_name='blogs'
+    )
+    name = models.CharField(
+        'Имя записи',
+        max_length=20
+    )
+    text = models.TextField(
+        'Текст',
+        max_length=100
+    )
+    create_date = models.DateField(
+        verbose_name='Дата заказа',
+        auto_now_add=True
+    )
+    publish = models.BooleanField(
+        'Опубликована ли запись',
+        default=False
+    )

5f9937ffd6ec7d4787316e1487a9fbe2a4df606a
Arseny <arsenyxiii@gmail.com>
2023-11-08 19:53:08 +0300

    add some test

--- tests/test_views.py
+++ tests/test_views.py
@@ -23,10 +23,12 @@ class TestBlogAPI:
             '`/blogs/` возвращается ответ со статусом 200.'
         )
 
-    def check_post_data(self,
-                        response_data,
-                        request_method_and_url,
-                        db_blog=None):
+    def check_blog_data(
+        self,
+        response_data,
+        request_method_and_url,
+        db_blog=None
+    ):
         expected_fields = ('id', 'name', 'text', 'user', 'create_date')
         for field in expected_fields:
             assert field in response_data, (
@@ -40,8 +42,20 @@ class TestBlogAPI:
                 '`id` блога.'
             )
 
+    def check_blog_data_without_user(
+        self, response_data,
+        request_method_and_url,
+        db_blog=None
+    ):
+        expected_fields = ('id', 'name', 'text', 'create_date')
+        for field in expected_fields:
+            assert field in response_data, (
+                'Проверьте, что для авторизованного пользователя ответ на '
+                f'{request_method_and_url} содержит поле `{field}` блогов.'
+            )
+
     @pytest.mark.django_db(transaction=True)
-    def test_posts_auth_get(self, user_client, blog, another_blog):
+    def test_blogs_auth_get(self, user_client, blog, another_blog):
         response = user_client.get('/blogs/')
         assert response.status_code == HTTPStatus.OK, (
             'Проверьте, что для авторизованного пользователя GET-запрос к '
@@ -61,8 +75,81 @@ class TestBlogAPI:
 
         db_blog = Blog.objects.first()
         test_blog = test_data[0]
-        self.check_post_data(
+        self.check_blog_data(
             test_blog,
             'GET-запрос к `/blogs/`',
             db_blog
+        )
+
+    @pytest.mark.django_db(transaction=True)
+    def test_blog_create_auth_with_invalid_data(self, user_client):
+        posts_count = Blog.objects.count()
+        response = user_client.post('/blogs/', data={})
+        assert response.status_code == HTTPStatus.BAD_REQUEST, (
+            'Проверьте, что для авторизованного пользователя POST-запрос с '
+            'некорректными данными к `/blogs/` возвращает ответ со '
+            'статусом 400.'
+        )
+        assert posts_count == Blog.objects.count(), (
+            'Проверьте, что POST-запрос к `/blogs/` с некорректными '
+            'данными не создает новый blog.'
+        )
+
+    @pytest.mark.django_db(transaction=True)
+    def test_blog_create_auth_with_valid_data(self, user_client, user):
+        post_count = Blog.objects.count()
+
+        data = {
+            'name': 'Blog номер 3',
+            'text': 'Blog номер 3',
+            'publish': False
+        }
+        response = user_client.post('/blogs/', data=data)
+        assert response.status_code == HTTPStatus.CREATED, (
+            'Проверьте, что для авторизованного пользователя  POST-запрос с '
+            'корректными данными к `/blogs/` возвращает ответ со '
+            'статусом 201.'
+        )
+
+        test_data = response.json()
+        assert isinstance(test_data, dict), (
+            'Проверьте, что для авторизованного пользователя POST-запрос к '
+            '`/blogs/` возвращает ответ, содержащий данные нового '
+            'поста в виде словаря.'
+        )
+        self.check_blog_data_without_user(test_data, 'POST-запрос к `/blogs/`')
+        assert test_data.get('text') == data['text'], (
+            'Проверьте, что для авторизованного пользователя POST-запрос к '
+            '`/blogs/` возвращает ответ, содержащий текст нового '
+            'поста в неизменном виде.'
+        )
+        assert test_data.get('user') is None, (
+            'Проверьте, что для авторизованного пользователя при создании '
+            'поста через POST-запрос к `/blogs/` ответ не содержит поле '
+            '`user` с именем пользователя, отправившего запрос.'
+        )
+        assert post_count + 1 == Blog.objects.count(), (
+            'Проверьте, что POST-запрос с корректными данными от '
+            'авторизованного пользователя к `/blogs/` создает новый '
+            'пост.'
+        )
+
+    @pytest.mark.django_db(transaction=True)
+    def test_blog_unauth_create(self, client, user, another_user):
+        posts_conut = Blog.objects.count()
+
+        data = {
+            'name': 'Blog номер 3',
+            'text': 'Blog номер 3',
+            'publish': False
+        }
+        response = client.post('/blogs/', data=data)
+        assert response.status_code == HTTPStatus.UNAUTHORIZED, (
+            'Проверьте, что POST-запрос неавторизованного пользователя к '
+            '`/blogs/` возвращает ответ со статусом 401.'
+        )
+
+        assert posts_conut == Blog.objects.count(), (
+            'Проверьте, что POST-запрос неавторизованного пользователя к '
+            '`/blogs/` не создает новый пост.'
         )
